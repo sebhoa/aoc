3b74ad13c90fa234714a62192598edffe1cfbaad
Sébastien Hoarau <seb.hoarau@univ-reunion.fr>
2021-12-10 16:47:17 +0400

    clean up

--- 10/10.py
+++ 10/10.py
@@ -1,10 +1,10 @@
 import sys
 
 FILE = 'input10.txt'
-OPEN = {'>':'<', ')':'(', '}':'{', ']':'['}
-CLOSE = {'<':'>', '(':')', '{':'}', '[':']'}
-CSCORE = {')':3, ']':57, '}':1197, '>':25137}
-ASCORE = {')':1, ']':2, '}':3, '>':4}
+CLOSE_TO_OPEN = {'>':'<', ')':'(', '}':'{', ']':'['}
+OPEN_TO_CLOSE = {'<':'>', '(':')', '{':'}', '[':']'}
+CSCORE = {')':3, ']':57, '}':1197, '>':25137} # info for Corrupted line SCORE calculation
+ASCORE = {')':1, ']':2, '}':3, '>':4}         # info for Autocompleted line SCORE calculation
 
 class Subsystem:
 
@@ -16,29 +16,33 @@ class Subsystem:
     def parse(self, line):
         pile = []
         corrupted_line = False
-        for c in line:
-            if c in OPEN:
-                if pile == [] or pile[-1] != OPEN[c]:
+        for delimiter in line:
+            if delimiter in CLOSE_TO_OPEN:
+                # délimiteur fermant lu et l'ouvrant correspondant n'est pas sur la pile
+                if pile == [] or pile[-1] != CLOSE_TO_OPEN[delimiter]: 
                     corrupted_line = True
-                    self.corrupted_score += CSCORE[c]
+                    self.corrupted_score += CSCORE[delimiter]
                     break
+                # ou alors il l'est et on dépile gentiment
                 else:
                     pile.pop()
+            # on a un ouvrant, on empile
             else:
-                pile.append(c)
+                pile.append(delimiter)
+        # on a fini de parser la ligne, on lance une complétion s'il y a lieu
         if not corrupted_line and pile != []:
             self.autocomplete(pile)
 
 
     def autocomplete(self, line):
         score = 0
-        for c in line[::-1]:
-            score = score * 5 + ASCORE[CLOSE[c]]
+        for delimiter in line[::-1]:
+            score = score * 5 + ASCORE[OPEN_TO_CLOSE[delimiter]]
         self.scores.append(score)
 
 
-
     def solve(self):
+        """Le même pour les 2 parties"""
         with open(self.filename, 'r', encoding='utf-8') as datas:
             for line in datas:
                 self.parse(line.strip())
@@ -56,9 +60,8 @@ def main():
         print(logs.corrupted_score)
     else:
         logs.scores.sort()
-        print(logs.scores)
-        n = len(logs.scores)
-        print(logs.scores[n//2])
+        middle = len(logs.scores) // 2
+        print(logs.scores[middle])
 
 
 if __name__ == '__main__':
