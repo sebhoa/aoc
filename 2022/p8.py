41eeacb03fae17a22375ab25f72ac9a1cf88f5c3
Sébastien Hoarau <seb.hoarau@univ-reunion.fr>
2022-12-08 17:12:04 +0400

    noms en anglais

--- 2022/p8.py
+++ 2022/p8.py
@@ -9,7 +9,7 @@ class P8(Puzzle):
 
     def __init__(self):
         Puzzle.__init__(self, 8)
-        self.grille = []
+        self.grid = []
         self.height = 0
         self.width = 0
         
@@ -17,32 +17,37 @@ class P8(Puzzle):
         if filename is None:
             filename = self.tests[part]
         with open(filename) as datas:
-            self.grille = [[int(e) for e in ligne.strip()] for ligne in datas]
-        self.height = len(self.grille)
-        self.width = len(self.grille[0])
+            self.grid = [[int(e) for e in line.strip()] for line in datas]
+        self.height = len(self.grid)
+        self.width = len(self.grid[0])
         
     def inside(self, i, j):
         return 0 <= i < self.height and 0 <= j < self.width
     
-    def bordure(self, i, j):
+    def on_edge(self, i, j):
         return i == 0 or i == self.height - 1 or j == 0 or j == self.width - 1
     
-    def visible_depuis(self, i0, j0, direction):
-        """Renvoie True si la case intérieure i0, j0 est visible depuis la direction donnée"""
+    def neighbors(self, i, j, direction):
+        """Un itérateur sur les voisins de i, j dans la direction donnée, en restant dans la zone"""
         di, dj = direction
-        i, j = i0, j0 
-        est_visible = True
-        while self.inside(i, j) and est_visible:
+        i, j = i + di, j + dj
+        while self.inside(i, j):
+            yield i, j
             i, j = i + di, j + dj
-            est_visible = not self.inside(i, j) or self.grille[i][j] < self.grille[i0][j0]
-        return est_visible
+            
+    def visible_from(self, i0, j0, direction):
+        """Renvoie True si la case intérieure i0, j0 est visible depuis la direction donnée"""
+        for i, j in self.neighbors(i0, j0, direction):
+            if self.grid[i][j] >= self.grid[i0][j0]:
+                return False
+        return True
         
-    def scenic_score_une_dir(self, i0, j0, direction):
+    def on_direction_scenic_score(self, i0, j0, direction):
         """Calcule la valeur scénique de la case intérieure i0, j0 dans une direction donnée"""
         di, dj = direction
         i, j = i0 + di, j0 + dj 
         scenic_score = 1
-        while self.inside(i, j) and self.grille[i][j] < self.grille[i0][j0]:
+        while self.inside(i, j) and self.grid[i][j] < self.grid[i0][j0]:
             scenic_score += 1
             i, j = i + di, j + dj
         if not self.inside(i, j):
@@ -50,12 +55,12 @@ class P8(Puzzle):
         return scenic_score
         
     def visible(self, i, j):
-        return self.bordure(i, j) or any(self.visible_depuis(i, j, d) for d in DIRECTIONS)
+        return self.on_edge(i, j) or any(self.visible_from(i, j, d) for d in DIRECTIONS)
 
     def scenic_score(self, i, j):
         prod = 1
         for d in DIRECTIONS:
-            prod *= self.scenic_score_une_dir(i, j, d)
+            prod *= self.on_direction_scenic_score(i, j, d)
         return prod
             
     def solve(self, part, filename=None):
--- README.md
+++ README.md
@@ -13,7 +13,7 @@ C'est parti. Comme d'habitude les premiers sont simples. Cette année tout dans 
 - jour 5 : ✅ ✅ (pénible sans regex)
 - jour 6 : ✅ ✅ 
 - jour 7 : ✅ ✅ (galère parce que je n'ai pas pensé que 2 répertoires pouvaient avoir le même nom)
-- jour 8 : ✅ ✅ 
+- jour 8 : ✅ ✅ (bon je repasse les noms en anglais ^^)
 
 
 ### Création du puzzle du jour $k$
