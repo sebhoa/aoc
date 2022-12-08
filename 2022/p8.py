6f8b29ac8e05c438329ad30f791cda7e85f209d9
Sébastien Hoarau <seb.hoarau@univ-reunion.fr>
2022-12-08 17:16:10 +0400

    noms en anglais

--- 2022/p8.py
+++ 2022/p8.py
@@ -27,6 +27,8 @@ class P8(Puzzle):
     def on_edge(self, i, j):
         return i == 0 or i == self.height - 1 or j == 0 or j == self.width - 1
     
+    # Part One ---
+
     def neighbors(self, i, j, direction):
         """Un itérateur sur les voisins de i, j dans la direction donnée, en restant dans la zone"""
         di, dj = direction
@@ -41,7 +43,12 @@ class P8(Puzzle):
             if self.grid[i][j] >= self.grid[i0][j0]:
                 return False
         return True
-        
+
+    def visible(self, i, j):
+        return self.on_edge(i, j) or any(self.visible_from(i, j, d) for d in DIRECTIONS)
+
+    # Part Two ---
+
     def on_direction_scenic_score(self, i0, j0, direction):
         """Calcule la valeur scénique de la case intérieure i0, j0 dans une direction donnée"""
         di, dj = direction
@@ -54,15 +61,14 @@ class P8(Puzzle):
             scenic_score -= 1
         return scenic_score
         
-    def visible(self, i, j):
-        return self.on_edge(i, j) or any(self.visible_from(i, j, d) for d in DIRECTIONS)
-
     def scenic_score(self, i, j):
         prod = 1
         for d in DIRECTIONS:
             prod *= self.on_direction_scenic_score(i, j, d)
         return prod
             
+    # Solve --- 
+    
     def solve(self, part, filename=None):
         self.load_datas(part, filename)
         if part % 2 == 0:
