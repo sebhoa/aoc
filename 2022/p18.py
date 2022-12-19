e7875791b3ee33c91265e705650b9248702cff51
Sébastien Hoarau <seb.hoarau@univ-reunion.fr>
2022-12-19 10:48:08 +0400

    Meilleure modélisation, propre

--- 2022/p18.py
+++ 2022/p18.py
@@ -21,6 +21,8 @@ A --------- B
 from collections import deque
 from puzzle import Puzzle
 
+# -- Points pour décrire un cube ou une face
+# --
 A = 0, 0, 0
 B = 1, 0, 0
 C = 1, 1, 0
@@ -32,23 +34,53 @@ H = 0, 1, 1
 
 POINTS = B, C, D, E, F, G, H
 
-DELTAS = (0, 0, -1), (1, 0, 0), (0, 0, 1), (-1, 0, 0), (0, -1, 0), (0, 1, 0)
+# -- Vecteurs unitaires
+# --
+UP = (0, 0, 1)
+DOWN = (0, 0, -1)
+LEFT = (-1, 0, 0)
+RIGHT = (1, 0, 0)
+FORWARD = (0, -1, 0)
+BACKWARD = (0, 1, 0)
+
+VECTORS = UP, DOWN, LEFT, RIGHT, FORWARD, BACKWARD
+
+# -- Type des cubes
+# --
+INSIDE = 0
+OUTSIDE = 1
+BORDER = 2
+LAVA = 3
+
+STR_TYPE = 'IN', 'OUT', 'BORD', 'LAVE'
+
+# -- Quelques petits utilitaires
+# -- 
+def first_or_last(n, integers):
+    return len(integers) == 0 or n <= integers[0] or n >= integers[-1]
+
+
+# -- LES CLASSES
+# --------------
 
 class Cube:
 
-    def __init__(self, puzzle, x, y, z):
+    def __init__(self, puzzle, x, y, z, type):
+        self.puzzle = puzzle
         self.x = x
         self.y = y
         self.z = z
-        self.puzzle = puzzle
-
+        self.type = type
+        self.is_outside()
+        
     def __repr__(self):
-        return f'Cube({self.x}, {self.y}, {self.z})'
+        return f'Cube({self.x}, {self.y}, {self.z}, {STR_TYPE[self.type]})'
 
     def origin(self):
         return self.x, self.y, self.z
 
     def faces(self):
+        """Renvoie un générateur sur les 6 faces d'un cube"""
         x, y, z = self.x, self.y, self.z
         return (tuple(sorted((x+dx, y+dy, z+dz) for dx, dy, dz in (A, B, C, D))), 
                 tuple(sorted((x+dx, y+dy, z+dz) for dx, dy, dz in (E, F, G, H))), 
@@ -58,9 +90,26 @@ class Cube:
                 tuple(sorted((x+dx, y+dy, z+dz) for dx, dy, dz in (D, A, E, H))))
 
     def neighbors(self):
-        return ((self.x + dx, self.y + dy, self.z + dz) for dx, dy, dz in DELTAS)
+        """"renvoie un générateur sur les 6 points voisins du point origine d'un cube (le point A cf schéma)"""
+        return ((self.x + dx, self.y + dy, self.z + dz) for dx, dy, dz in VECTORS)
 
-    
+    def is_outside(self):
+        if self.type != LAVA and self.infinity_sight():
+            self.type = OUTSIDE
+            return True
+        return False
+
+    def infinity_sight(self):
+        """Renvoie True si le cube à une vue vers l'infini dans une des 6 directions"""
+        cubes = self.puzzle.cubes.values()
+        x_with_same_yz = sorted(cube.x for cube in cubes if cube.y == self.y and cube.z == self.z)
+        y_with_same_xz = sorted(cube.y for cube in cubes if cube.x == self.x and cube.z == self.z)
+        z_with_same_xy = sorted(cube.z for cube in cubes if cube.x == self.x and cube.y == self.y)
+
+        return first_or_last(self.x, x_with_same_yz) or\
+                first_or_last(self.y, y_with_same_xz) or\
+                first_or_last(self.z, z_with_same_xy)
+
 
 class P18(Puzzle):
 
@@ -68,100 +117,84 @@ class P18(Puzzle):
         Puzzle.__init__(self, 18, part)
         self.cubes = {}
         self.faces = {}
-        self.air = {}
-        self.trapped_air = set()
-        self.outside = None
-
-    def set_outside(self):
-        xs = set(origin[0] for origin in self.cubes)
-        ys = set(origin[1] for origin in self.cubes)
-        zs = set(origin[2] for origin in self.cubes)
-        min_x, max_x = min(xs), max(xs)
-        min_y, max_y = min(ys), max(ys)
-        min_z, max_z = min(zs), max(zs)
-        self.outside = (min_x, max_x, min_y, max_y, min_z, max_z)
-
 
     def load_datas(self, filename):
         with open(filename) as datas:
             for line in datas:
                 x, y, z = [int(e) for e in line.strip().split(',')]
-                self.cubes[x, y, z] = Cube(self, x, y, z)
-        self.set_outside()
+                self.cubes[x, y, z] = Cube(self, x, y, z, LAVA)
 
     def reset(self):
         self.cubes = {}
         self.faces = {}
-        self.air = {}
-        self.trapped_air = set()
-        self.free = set()
 
-    def constant_component(self, face):
-        for i in (0, 1, 2):
-            if all(face[k][i] == face[0][i] for k in range(len(face))):
-                return i
+    def set_faces(self):
+        """Comptabilise toutes les faces de tous les cubes de type lave"""
+        for cube in self.cubes.values():
+            if cube.type == LAVA:
+                for face in cube.faces():
+                    self.faces[face] = self.faces.get(face, 0) + 1
 
-    def is_fresh_air(self, x, y, z):
-        out = self.outside
-        return x <= out[0] or x >= out[1] or y <= out[2] or y >= out[3] or z <= out[4] or z >= out[5]
+    def border(self):
+        return (cube for cube in self.cubes.values() if cube.type == BORDER)
 
-    def air_cubes(self):
-        origins = deque(list(self.cubes.keys()))
-        visited = set()
-        while len(origins) > 0:
-            origin = origins.popleft()
-            visited.add(origin)
-            if origin not in self.cubes:
-                if not self.is_fresh_air(*origin):
-                    self.air[origin] = Cube(self, *origin)
-                    for neighbor in self.air[origin].neighbors():
-                        if neighbor not in origins and neighbor not in visited:
-                            origins.append(neighbor)
-            else:
-                for neighbor in self.cubes[origin].neighbors():
-                    if neighbor not in origins and neighbor not in visited:
-                        origins.append(neighbor)
+    def outside(self):
+        return (cube for cube in self.cubes.values() if cube.type == OUTSIDE)
 
-    def update_trapped(self, origin):
-        """retire de l'ensemble de cubes d'air supposés emprisonnés, ceux qui ne le sont pas"""
-        cubes_to_explore = deque([origin])
-        visited = set()
-        free = False
-        while len(cubes_to_explore) > 0:
-            x, y, z = cubes_to_explore.popleft()
-            visited.add((x, y, z))
-            if self.is_fresh_air(x, y, z):
-                free = True
-            else:
-                for dx, dy, dz in DELTAS:
-                    neighbor = x+dx, y+dy, z+dz
-                    if neighbor not in self.cubes and neighbor not in self.free and neighbor not in cubes_to_explore and neighbor not in visited:
-                        cubes_to_explore.append(neighbor)
-        if free:
-            for origin in visited:
-                self.trapped_air.discard(origin)
-                self.free.add(origin)
+    def inside(self):
+        return (cube for cube in self.cubes.values() if cube.type == INSIDE)
+
+    def set_border(self):
+        """Un parcours en largeur à partir des cubes lave pour déterminer les cubes frontière"""
+        new_cubes = {}
+        for cube in self.cubes.values():
+            for xyz in cube.neighbors():
+                if xyz not in self.cubes and xyz not in new_cubes:
+                    new_cubes[xyz] = Cube(self, *xyz, BORDER)
+        self.cubes.update(new_cubes)
+
+    def set_inside(self):
+        """La méthode la plus compliquée de la modélisation : on parcourt en largeur
+        tous les cubes de type BORDER pour obtenir la composante connexe... si un des
+        cubes est OUTSIDE alors ils le sont tous. Sinon c'est du INSIDE"""
+        for cube in self.cubes.values():
+            if cube.type == BORDER:
+                to_explore = deque([cube.origin()])
+                connected_xyz = set()
+                type_of_connected = INSIDE
+                while len(to_explore) > 0:
+                    current_xyz = to_explore.popleft()
+                    cube = self.cubes[current_xyz]
+                    connected_xyz.add(current_xyz)
+                    if cube.is_outside():
+                        type_of_connected = OUTSIDE
+                    else:
+                        for xyz in cube.neighbors():
+                            if xyz in self.cubes and self.cubes[xyz].type != LAVA and xyz not in to_explore and xyz not in connected_xyz:
+                                to_explore.append(xyz)
+                for xyz in connected_xyz:
+                    self.cubes[xyz].type = type_of_connected
+
 
     def solve(self, filename, *args):
         self.reset()
+        if len(args) > 0:
+            filename = args[0]
         self.load_datas(filename)
-        for cube in self.cubes.values():
-            for face in cube.faces():
-                self.faces[face] = self.faces.get(face, 0) + 1
+        self.set_faces()
+        self.solution = sum(nb for nb in self.faces.values() if nb == 1)
 
-        if self.part == 0:
-            self.solution = sum(nb for nb in self.faces.values() if nb == 1)
-        else:
-            self.air_cubes()
+        if self.part == 1:
+            # pour la Part Two (part = 1) on doit supprimer quelques faces avec de compter
+            #
+            self.set_border()
+            self.set_inside()
+            for cube in self.cubes.values():
+                if cube.type == INSIDE:
+                    for f in cube.faces():
+                        self.faces[f] = 0
 
-            self.trapped_air = set(self.air)
-            for origin in self.air:
-                self.update_trapped(origin)
-            for origin in self.trapped_air:
-                for f in self.air[origin].faces():
-                    self.faces[f] = 0
-
-            self.solution = sum(nb for nb in self.faces.values() if nb == 1)
+        self.solution = sum(nb for nb in self.faces.values() if nb == 1)
 
 
 
@@ -169,14 +202,14 @@ class P18(Puzzle):
 
 p_one = P18(0)
 
-# p_one.test()
-# print(p_one)
-# p_one.validate()
-# print(p_one)
+p_one.test()
+print(p_one)
+p_one.validate()
+print(p_one)
 
 p_two = P18(1)
-# p_two.test()
-# print(p_two)
+p_two.test()
+print(p_two)
 p_two.validate()
 print(p_two)
 
